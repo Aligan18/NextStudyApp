ab1c76e3589059c1c90f97bda64431e71d4bfff0
Aligan18 <alisherganiev18@gmail.com>
2023-04-27 10:59:36 +0600

    I added Routing in the Menu Component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,14 +4,16 @@ import { ETopLevelCategory } from '@/interfaces/page.interface'
 import React from 'react'
 import cn from 'classnames'
 import classes from "./Menu.module.scss"
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 
 const Menu = () => {
+  
+  const {menu,firstCategory,setMenu} = useMenuContext()
+  const router = useRouter()
+  
 
-const {menu,firstCategory,setMenu} = useMenuContext()
-
-  const secondCategory ="Бизнес"
-  const thirdCategory = ""
 
   const menuArray : IFirstLevelMenuItem[] = 
     [ {title:"Курсы",id: ETopLevelCategory.Courses , icon:<i className="fa-solid fa-graduation-cap"></i>, route : "courses" }, 
@@ -20,18 +22,32 @@ const {menu,firstCategory,setMenu} = useMenuContext()
       {title:"Товары", id:ETopLevelCategory.Products ,icon:<i className="fa-solid fa-box-open"></i>, route : "courses" }, 
   ]
 
+  const changeIsOpened =(secondCategory:string)=>{
+    setMenu && setMenu(menu.map(m=>{
+        if(m._id.secondCategory===secondCategory){
+         m.isOpened= !m.isOpened
+        }
+        return m
+    }))
+  }
+
   const renderFirstList =()=>{
+
+
+
     return <>
                   {menuArray.map(menuItem=>
                       <div key={menuItem.id}>
-                          <a href ={`/${menuItem.route}`}>
+                        <Link href ={`/${menuItem.route}`}>
+                          <div >
                             <div className={cn(classes.first_category ,{
                                               [classes.first_category_active] : firstCategory === menuItem.id
                             })}>
                                 {menuItem.icon}
                                 <span>{menuItem.title}</span>
                             </div> 
-                          </a>
+                          </div>
+                        </Link>
                           {menuItem.id === firstCategory && renderSecondList(menuItem)}
                       </div>
                   )}
@@ -39,18 +55,30 @@ const {menu,firstCategory,setMenu} = useMenuContext()
 
   }
   const renderSecondList =(menuItem:IFirstLevelMenuItem)=>{
+
+    
+
     return(
           <div>
-              {menu.map((m)=>
-                    <div key={m._id.secondCategory}>
-                        <div className={classes.second_category}> {m._id.secondCategory}</div>
+              {menu.map((m)=>{
+                  if (m._id.secondCategory.includes(router.asPath.split("/")[2])){
+                      m.isOpened= true
+                  }
+                  return (
+
+                    <div key={m._id.secondCategory}  >
+                        <div  className={cn (classes.second_category,
+                                              {[classes.second_category_selected] : m.isOpened})} 
+                              onClick={()=>changeIsOpened(m._id.secondCategory)}> 
+                                     {m._id.secondCategory}
+                        </div>
                         <div className={cn(classes.second_category_block ,classes.left_border , {
                               [classes.second_category_block_opened] : m.isOpened 
                         })}>
                             {  renderThirdList(m.pages, menuItem.route)}
                         </div>
                     </div>
-              )}
+              )})}
           </div>
         ) 
 
@@ -59,21 +87,21 @@ const {menu,firstCategory,setMenu} = useMenuContext()
 
   const renderThirdList =(pages :IPageItem[],route:string)=>{
     return <div className={classes.thrid_category_hover}>
-              {pages.map((p)=><a  href={`/${route}/${p.alias}`} 
-                                  key={p._id}
-                                  className={cn(classes.thrid_category,{
-                                        [classes.thrid_category_active]:false
-                                  })}
-                                  >
-                                      {p.title}
-                              </a>
+              {pages.map((p)=><Link key={p._id} href={`/${route}/${p.alias}`}>
+                                  <div  className={cn(classes.thrid_category,{
+                                                      [classes.thrid_category_active]: router.asPath === `/${route}/${p.alias}`
+                                                })}
+                                    >
+                                        {p.title}
+                                </div>
+                              </Link>
               )}
           </div>
   }
 
 
   
- console.log(firstCategory)
+
   return (
     <div>
         {renderFirstList()}
