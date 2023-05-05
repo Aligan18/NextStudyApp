5ccaa556bcefb3c8822e42f1648301c92bf1f8f7
Aligan18 <alisherganiev18@gmail.com>
2023-05-05 19:59:10 +0600

    Deploying to Vercel

--- src/helpers/firstLevelMenu.tsx
+++ src/helpers/firstLevelMenu.tsx
@@ -6,7 +6,7 @@ import { ETopLevelCategory } from "@/interfaces/page.interface";
 
 export const firstLevelMenu : IFirstLevelMenuItem[] = 
   [ {title:"Курсы",id: ETopLevelCategory.Courses , icon:<i className="fa-solid fa-graduation-cap"></i>, route : "courses" }, 
-    {title:"Сервесы", id: ETopLevelCategory.Services, icon:<i className="fa-solid fa-cloud"></i>, route : "services" }, 
-    {title:"Книги", id:ETopLevelCategory.Books, icon:<i className="fa-solid fa-book"></i>, route : "books" }, 
-    {title:"Товары", id:ETopLevelCategory.Products ,icon:<i className="fa-solid fa-box-open"></i>, route : "products" }, 
+    // {title:"Сервесы", id: ETopLevelCategory.Services, icon:<i className="fa-solid fa-cloud"></i>, route : "services" }, 
+    // {title:"Книги", id:ETopLevelCategory.Books, icon:<i className="fa-solid fa-book"></i>, route : "books" }, 
+    // {title:"Товары", id:ETopLevelCategory.Products ,icon:<i className="fa-solid fa-box-open"></i>, route : "products" }, 
 ]
